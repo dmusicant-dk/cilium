dmusicant-dk/cilium

commit 6c06c51926bcebd71fa23fadbb76801a0f1f62ec

node: Remove permanent ARP entry when remote node is deleted
When IPsec or NodePort is enabled, we add a permanent ARP entry (remote
node IP => remote node MAC addr) upon receiving a NodeUpdate event.
The entry is needed to facilitate calls to fib_lookup() from the
datapath.

Up until now, the permanent entry was not removed when the remote node
was deleted. This could lead to a problem, when a packet destined to a
new node which reused the IP addr of the deleted node was dropped due to
the wrong MAC addr until NodeUpdate event for the new node had been
received.

This commit fixes the problem by removing obsolete ARP entries.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/node.go
@@ -42,12 +42,14 @@ const (
 )
 
 type linuxNodeHandler struct {
-	mutex          lock.Mutex
-	isInitialized  bool
-	nodeConfig     datapath.LocalNodeConfiguration
-	nodeAddressing datapath.NodeAddressing
-	datapathConfig DatapathConfiguration
-	nodes          map[node.Identity]*node.Node
+	mutex                lock.Mutex
+	isInitialized        bool
+	nodeConfig           datapath.LocalNodeConfiguration
+	nodeAddressing       datapath.NodeAddressing
+	datapathConfig       DatapathConfiguration
+	nodes                map[node.Identity]*node.Node
+	enableNeighDiscovery bool
+	neighByNode          map[node.Identity]*netlink.Neigh
 }
 
 // NewNodeHandler returns a new node handler to handle node events and
@@ -57,6 +59,7 @@ func NewNodeHandler(datapathConfig DatapathConfiguration, nodeAddressing datapat
 		nodeAddressing: nodeAddressing,
 		datapathConfig: datapathConfig,
 		nodes:          map[node.Identity]*node.Node{},
+		neighByNode:    map[node.Identity]*netlink.Neigh{},
 	}
 }
 
@@ -543,7 +546,7 @@ func neighborLog(spec, iface string, err error, ip *net.IP, hwAddr *net.Hardware
 	scopedLog := log.WithFields(logrus.Fields{
 		logfields.Reason: spec,
 		"Interface":      iface,
-		"IP":             ip,
+		logfields.IPAddr: ip,
 		"HardwareAddr":   hwAddr,
 		"LinkIndex":      link,
 	})
@@ -592,8 +595,26 @@ func (n *linuxNodeHandler) insertNeighbor(newNode *node.Node, ifaceName string) 
 		}
 		err := netlink.NeighSet(&neigh)
 		neighborLog("insertNeighbor NeighSet", ifaceName, err, &ciliumIPv4, &hwAddr, link)
+		if err == nil {
+			n.neighByNode[newNode.Identity()] = &neigh
+		}
 	} else {
 		neighborLog("insertNeighbor arping failed", ifaceName, err, &ciliumIPv4, &hwAddr, link)
+	}
+}
+
+func (n *linuxNodeHandler) deleteNeighbor(oldNode *node.Node) {
+	neigh, ok := n.neighByNode[oldNode.Identity()]
+	if !ok {
+		return
+	}
+
+	if err := netlink.NeighDel(neigh); err != nil {
+		log.WithFields(logrus.Fields{
+			logfields.IPAddr: neigh.IP,
+			"HardwareAddr":   neigh.HardwareAddr,
+			"LinkIndex":      neigh.LinkIndex,
+		}).WithError(err).Warn("Failed to remove neighbor entry")
 	}
 }
 
@@ -676,8 +697,7 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *node.Node, firstAddition
 		newKey = newNode.EncryptionKey
 	}
 
-	if n.nodeConfig.EnableIPv4 && (option.Config.EnableNodePort ||
-		(n.nodeConfig.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled)) {
+	if n.enableNeighDiscovery {
 		var ifaceName string
 		if option.Config.EnableNodePort {
 			ifaceName = option.Config.Device
@@ -776,6 +796,10 @@ func (n *linuxNodeHandler) nodeDelete(oldNode *node.Node) error {
 			n.deleteNodeRoute(oldNode.IPv4AllocCIDR)
 			n.deleteNodeRoute(oldNode.IPv6AllocCIDR)
 		}
+	}
+
+	if n.enableNeighDiscovery {
+		n.deleteNeighbor(oldNode)
 	}
 
 	if n.nodeConfig.EnableIPSec {
@@ -1074,6 +1098,10 @@ func (n *linuxNodeHandler) NodeConfigurationChanged(newConfig datapath.LocalNode
 
 	prevConfig := n.nodeConfig
 	n.nodeConfig = newConfig
+
+	n.enableNeighDiscovery = n.nodeConfig.EnableIPv4 &&
+		(option.Config.EnableNodePort ||
+			(n.nodeConfig.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled))
 
 	n.updateOrRemoveNodeRoutes(prevConfig.AuxiliaryPrefixes, newConfig.AuxiliaryPrefixes)
 

commit a467806e74168bc45a0fad25c1663dbebacf7433

remove ARP entries left from previous Cilium run
In certain configurations, when node neighbor discovery is enabled [1],
the neighbor table is populated with PERMANENT entries. If the agent is
then configured to not use neighbor discovery, those entries are left
behind, without being garbage collected. This can cause connectivity
issues across nodes, where it's more likely to happen in the same L2
network, if a new node reuses an IP address from a previous node and its
MAC address changes. In a L3 network it is unlikely to happen since the
ARP entry will be associated with a L3 router and it is less likely to
change its MAC address.

[1]
```
n.enableNeighDiscovery = n.nodeConfig.EnableIPv4 &&
	(option.Config.EnableNodePort ||
		(n.nodeConfig.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled))
```

Fixes: 6c06c51926bc ("node: Remove permanent ARP entry when remote node is deleted")
Signed-off-by: André Martins <andre@cilium.io>

daemon/cmd/daemon_main.go
@@ -389,6 +389,9 @@ func init() {
 	flags.Bool(option.ForceLocalPolicyEvalAtSource, defaults.ForceLocalPolicyEvalAtSource, "Force policy evaluation of all local communication at the source endpoint")
 	option.BindEnv(option.ForceLocalPolicyEvalAtSource)
 
+	flags.Bool(option.GCNeighEntries, false, "Garbage collects, on initialization, L2 entries created by a previous agent if neighbor discovery was enabled")
+	option.BindEnv(option.GCNeighEntries)
+
 	flags.String(option.HTTP403Message, "", "Message returned in proxy L7 403 body")
 	flags.MarkHidden(option.HTTP403Message)
 	option.BindEnv(option.HTTP403Message)
@@ -1475,6 +1478,13 @@ func runDaemon() {
 	// Start periodical arping to refresh neighbor table
 	if d.datapath.Node().NodeNeighDiscoveryEnabled() {
 		d.nodeDiscovery.Manager.StartNeighborRefresh(d.datapath.Node())
+	} else {
+		// clean up all arp PERM entries that might have previously set by
+		// a Cilium instance
+		if option.Config.GCNeighEntries {
+			log.Info("Deleting old neighbor entries left by previous agent")
+			d.datapath.Node().NodeCleanNeighbors()
+		}
 	}
 
 	log.WithField("bootstrapTime", time.Since(bootstrapTimestamp)).

daemon/cmd/status.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020 Authors of Cilium
+// Copyright 2016-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -315,6 +315,11 @@ func (c *clusterNodesClient) NodeNeighDiscoveryEnabled() bool {
 }
 
 func (c *clusterNodesClient) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) {
+	// no-op
+	return
+}
+
+func (c *clusterNodesClient) NodeCleanNeighbors() {
 	// no-op
 	return
 }

pkg/datapath/linux/node.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1362,6 +1362,52 @@ func (n *linuxNodeHandler) NodeNeighborRefresh(ctx context.Context, nodeToRefres
 			return
 		case <-refreshComplete:
 			return
+		}
+	}
+}
+
+// NodeCleanNeighbors cleans all neighbor entries for the direct routing device
+// and the encrypt interface. It should be used when the agent changes the
+// state from `n.enableNeighDiscovery = true` to
+// `n.enableNeighDiscovery = false`.
+func (n *linuxNodeHandler) NodeCleanNeighbors() {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	linksToClean := []string{option.Config.DirectRoutingDevice}
+	if option.Config.EncryptInterface != option.Config.DirectRoutingDevice {
+		linksToClean = append(linksToClean, option.Config.EncryptInterface)
+	}
+
+	for _, linkName := range linksToClean {
+		l, err := netlink.LinkByName(linkName)
+		if err != nil {
+			if _, ok := err.(netlink.LinkNotFoundError); !ok {
+				log.WithError(err).WithFields(logrus.Fields{
+					logfields.Device: linkName,
+				}).Error("Unable to GC old neighbor entries for network device")
+			}
+			continue
+		}
+
+		neighList, err := netlink.NeighListExecute(netlink.Ndmsg{
+			Family: netlink.FAMILY_V4,
+			Index:  uint32(l.Attrs().Index),
+			State:  netlink.NUD_PERMANENT,
+		})
+		if err != nil {
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.Device:    linkName,
+				logfields.LinkIndex: l.Attrs().Index,
+			}).Error("Unable to GC old neighbor entries for network device")
+			continue
+		}
+		for _, neigh := range neighList {
+			err := netlink.NeighDel(&neigh)
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.Device:    linkName,
+				logfields.LinkIndex: l.Attrs().Index,
+				"neighbor":          neigh.String(),
+			}).Error("Unable to GC old neighbor entry for network device")
 		}
 	}
 }

pkg/datapath/node.go
@@ -140,4 +140,8 @@ type NodeHandler interface {
 
 	// NodeNeighborRefresh is called to refresh node neighbor table
 	NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node)
+
+	// NodeCleanNeighbors cleans all neighbor entries for the direct routing device
+	// and the encrypt interface.
+	NodeCleanNeighbors()
 }

pkg/hubble/peer/handler.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Authors of Cilium
+// Copyright 2020-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -137,6 +137,11 @@ func (h handler) NodeNeighDiscoveryEnabled() bool {
 // NodeNeighborRefresh implements
 // datapath.NodeHandler.NodeNeighborRefresh. It is a no-op.
 func (h handler) NodeNeighborRefresh(_ context.Context, _ types.Node) {
+	// no-op
+	return
+}
+
+func (h handler) NodeCleanNeighbors() {
 	// no-op
 	return
 }

pkg/option/config.go
@@ -550,6 +550,10 @@ const (
 	// load loggging
 	LogSystemLoadConfigName = "log-system-load"
 
+	// GCNeighEntries is used to GC neighbor entries left by a previous running
+	// agent.
+	GCNeighEntries = "neighbor-garbage-collect"
+
 	// PrependIptablesChainsName is the name of the option to enable
 	// prepending iptables chains instead of appending
 	PrependIptablesChainsName = "prepend-iptables-chains"
@@ -1418,6 +1422,10 @@ type DaemonConfig struct {
 	// NeighMapEntriesGlobal is the maximum number of neighbor mappings
 	// allowed in the BPF neigh table
 	NeighMapEntriesGlobal int
+
+	// GCNeighEntries is used to GC neighbor entries left by a previous running
+	// agent.
+	GCNeighEntries bool
 
 	// PolicyMapEntries is the maximum number of peer identities that an
 	// endpoint may allow traffic to exchange traffic with.
@@ -2384,6 +2392,7 @@ func (c *DaemonConfig) Populate() {
 	c.EncryptNode = viper.GetBool(EncryptNode)
 	c.EnvoyLogPath = viper.GetString(EnvoyLog)
 	c.ForceLocalPolicyEvalAtSource = viper.GetBool(ForceLocalPolicyEvalAtSource)
+	c.GCNeighEntries = viper.GetBool(GCNeighEntries)
 	c.HostDevice = getHostDevice()
 	c.HTTPIdleTimeout = viper.GetInt(HTTPIdleTimeout)
 	c.HTTPMaxGRPCTimeout = viper.GetInt(HTTPMaxGRPCTimeout)

pkg/node/manager/manager_test.go
@@ -146,6 +146,10 @@ func (n *signalNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeTy
 	return
 }
 
+func (n *signalNodeHandler) NodeCleanNeighbors() {
+	return
+}
+
 func (s *managerTestSuite) TestNodeLifecycle(c *check.C) {
 	dp := newSignalNodeHandler()
 	dp.EnableNodeAddEvent = true